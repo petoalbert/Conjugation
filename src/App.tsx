3216f948673fb353a6bf7c53f7410d1da5efaf4b
berci <petoalbert32@gmail.com>
2024-05-01 12:05:41 +0200

    Format new files

--- src/App.tsx
+++ src/App.tsx
@@ -10,35 +10,35 @@ type MyButtonProps = {
 }
 
 const verbs = [
-    "actuar",
-    "acentuar",
-    "anticuar",
-    "atenuar",
-    "conceptuar",
-    "consensuar",
-    "continuar",
-    "desconceptuar",
-    "deshabituar",
-    "desvirtuar",
-    "devaluar",
-    "efectuar",
-    "evaluar",
-    "exceptuar",
-    "extenuar",
-    "fluctuar",
-    "graduar",
-    "habituar",
-    "insinuar",
-    "interactuar",
-    "menstruar",
-    "perpetuar",
-    "puntuar",
-    "redituar",
-    "reevaluar",
-    "revaluar",
-    "situar",
-    "tatuar",
-    "valuar"
+  "actuar",
+  "acentuar",
+  "anticuar",
+  "atenuar",
+  "conceptuar",
+  "consensuar",
+  "continuar",
+  "desconceptuar",
+  "deshabituar",
+  "desvirtuar",
+  "devaluar",
+  "efectuar",
+  "evaluar",
+  "exceptuar",
+  "extenuar",
+  "fluctuar",
+  "graduar",
+  "habituar",
+  "insinuar",
+  "interactuar",
+  "menstruar",
+  "perpetuar",
+  "puntuar",
+  "redituar",
+  "reevaluar",
+  "revaluar",
+  "situar",
+  "tatuar",
+  "valuar"
 ]
 
 function getVerb() {
@@ -69,12 +69,12 @@ function getTense(): Tense {
 
 function getPrononun(): Pronoun {
   const pronouns = [
-    Pronoun.Yo, 
-    Pronoun.Tu, 
-    Pronoun.Usted, 
-    Pronoun.Nosotros, 
-    Pronoun.Vosotros, 
-    Pronoun.Ustedes  
+    Pronoun.Yo,
+    Pronoun.Tu,
+    Pronoun.Usted,
+    Pronoun.Nosotros,
+    Pronoun.Vosotros,
+    Pronoun.Ustedes
   ]
   const randomIndex = Math.floor(Math.random() * pronouns.length);
   return pronouns[randomIndex];
@@ -89,36 +89,36 @@ function getParameters(): ConjugationParameters {
 }
 
 function Profile() {
-    return (
-      <img
-        src="https://i.imgur.com/MK3eW3As.jpg"
-        alt="Katherine Johnson"
-      />
-    );
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
+
+function MyButton({ onClick }: { onClick: () => void }) {
+  return (
+    <button onClick={onClick}>
+      New word
+    </button>
+  );
+}
+
+export default function Gallery() {
+  const [params, setParams] = useState<ConjugationParameters>(getParameters());
+
+  function handleClick() {
+    setParams(getParameters());
   }
 
-  function MyButton( { onClick } : { onClick: () => void } ) {  
-    return (
-      <button onClick={onClick}>
-        New word
-      </button>
-    );
-  }
-  
-  export default function Gallery() {
-    const [params, setParams] = useState<ConjugationParameters>(getParameters());
-  
-    function handleClick() {
-      setParams(getParameters());
-    }
-
-    return (
-      <section>
-        <MyButton onClick={handleClick} />
-        <div>{params.verb}</div>
-        <div>{tenseToString(params.tense)}</div>
-        <div>{pronounToString(params.pronoun)}</div>
-        <div>{conjugate(params.verb, params.tense, params.pronoun)}</div>
-      </section>
-    );
-  }
+  return (
+    <section>
+      <MyButton onClick={handleClick} />
+      <div>{params.verb}</div>
+      <div>{tenseToString(params.tense)}</div>
+      <div>{pronounToString(params.pronoun)}</div>
+      <div>{conjugate(params.verb, params.tense, params.pronoun)}</div>
+    </section>
+  );
+}
