1dc5fcf732710f59bc178a04a27b49871f095b42
berci <petoalbert32@gmail.com>
2024-05-01 12:01:17 +0200

    Add prononunToString

--- src/Pronoun.ts
+++ src/Pronoun.ts
@@ -1,3 +1,14 @@
 export enum Pronoun {
      Yo, Tu, Usted, Nosotros, Vosotros, Ustedes
 }
+
+export function pronounToString(pronoun: Pronoun): string {
+     switch (pronoun) {
+          case Pronoun.Yo: return "yo"
+          case Pronoun.Tu: return "tu"
+          case Pronoun.Usted: return "usted"
+          case Pronoun.Nosotros: return "nosotros"
+          case Pronoun.Vosotros: return "vosotros"
+          case Pronoun.Ustedes: return "ustedes"
+     }
+}
