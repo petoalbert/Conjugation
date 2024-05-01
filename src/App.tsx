06d2d00bdda098be6adc6b553d08e855e3d96dd8
berci <petoalbert32@gmail.com>
2024-05-01 22:43:47 +0200

    Move ConjugationParameters to new class

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import { conjugate } from "./Conjugation";
-import { Tense, tenseToString } from "./Tense";
-import { Pronoun, pronounToString } from "./Pronoun";
-import { verbs } from "./IrregularVerbs";
-import { Irregularities } from "./irregularities/Irregularity";
-
-const irregularities = new Map<Irregularities, string[]>();
-
-// Iterate over the original map's entries
-verbs.forEach((value, key) => {
-  // If the value is already in the reversed map, add the key to its array
-  if (irregularities.has(value)) {
-    irregularities.get(value)!.push(key);
-  } else {
-    // Otherwise, create a new array with the key as the only element
-    irregularities.set(value, [key]);
-  }
-});
-
-function getRandom<T>(l: T[]): T {
-  const randomIndex = Math.floor(Math.random() * l.length);
-  return l[randomIndex]
-}
-
-function getVerb():string {
-  return getRandom(getRandom(Array.from(irregularities.values())))
-}
-
-type ConjugationParameters = {
-  verb: string;
-  tense: Tense;
-  pronoun: Pronoun;
-}
-
-function getTense(): Tense {
-  const tenses = [
-    Tense.IndicativoPresente,
-    Tense.IndicativoImperfecto,
-    Tense.IndicativoIndefinido,
-    Tense.IndicativoFuturoImperfecto,
-    Tense.IndicativoCondicional,
-    Tense.SubjuntivoPresente,
-    Tense.ImperativoAfirmativo,
-    Tense.ImperativoNegativo
-  ]
-  const randomIndex = Math.floor(Math.random() * tenses.length);
-  return tenses[randomIndex];
-}
-
-function getPrononun(): Pronoun {
-  const pronouns = [
-    Pronoun.Yo,
-    Pronoun.Tu,
-    Pronoun.Usted,
-    Pronoun.Nosotros,
-    Pronoun.Vosotros,
-    Pronoun.Ustedes
-  ]
-  const randomIndex = Math.floor(Math.random() * pronouns.length);
-  return pronouns[randomIndex];
-}
-
-function getParameters(): ConjugationParameters {
-  return {
-    verb: getVerb(),
-    tense: getTense(),
-    pronoun: getPrononun()
-  }
-}
-
-function getOnlyIrregularParameters(): ConjugationParameters {
-  const verb = getVerb()
-  let forms = verbs.get(verb)?.forms(verb)
-  let tense = getRandom(Array.from(forms?.keys() ?? []))
-  let pronoun = getRandom(Array.from(forms?.get(tense)?.keys() ?? []))
-  // TODO find better way to handle undefined ^^^
-
-  return {
-    verb: verb,
-    tense: tense,
-    pronoun: pronoun
-  }
-}
+import { tenseToString } from "./Tense";
+import { pronounToString } from "./Pronoun";
+import { ConjugationParameters, getOnlyIrregularParameters, getParameters } from "./ConjugationParameters";
 
 function MyButton({ onClick }: { onClick: () => void }) {
   return (
---
+++ src/ConjugationParameters.ts
@@ -0,0 +1,82 @@
+import { Tense } from "./Tense";
+import { Pronoun } from "./Pronoun";
+import { verbs } from "./IrregularVerbs";
+import { Irregularities } from "./irregularities/Irregularity";
+
+const irregularities = new Map<Irregularities, string[]>();
+
+// Iterate over the original map's entries
+verbs.forEach((value, key) => {
+  // If the value is already in the reversed map, add the key to its array
+  if (irregularities.has(value)) {
+    irregularities.get(value)!.push(key);
+  } else {
+    // Otherwise, create a new array with the key as the only element
+    irregularities.set(value, [key]);
+  }
+});
+
+function getRandom<T>(l: T[]): T {
+  const randomIndex = Math.floor(Math.random() * l.length);
+  return l[randomIndex]
+}
+
+function getVerb():string {
+  return getRandom(getRandom(Array.from(irregularities.values())))
+}
+
+export type ConjugationParameters = {
+  verb: string;
+  tense: Tense;
+  pronoun: Pronoun;
+}
+
+function getTense(): Tense {
+  const tenses = [
+    Tense.IndicativoPresente,
+    Tense.IndicativoImperfecto,
+    Tense.IndicativoIndefinido,
+    Tense.IndicativoFuturoImperfecto,
+    Tense.IndicativoCondicional,
+    Tense.SubjuntivoPresente,
+    Tense.ImperativoAfirmativo,
+    Tense.ImperativoNegativo
+  ]
+  const randomIndex = Math.floor(Math.random() * tenses.length);
+  return tenses[randomIndex];
+}
+
+function getPrononun(): Pronoun {
+  const pronouns = [
+    Pronoun.Yo,
+    Pronoun.Tu,
+    Pronoun.Usted,
+    Pronoun.Nosotros,
+    Pronoun.Vosotros,
+    Pronoun.Ustedes
+  ]
+  const randomIndex = Math.floor(Math.random() * pronouns.length);
+  return pronouns[randomIndex];
+}
+
+export function getParameters(): ConjugationParameters {
+  return {
+    verb: getVerb(),
+    tense: getTense(),
+    pronoun: getPrononun()
+  }
+}
+
+export function getOnlyIrregularParameters(): ConjugationParameters {
+  const verb = getVerb()
+  let forms = verbs.get(verb)?.forms(verb)
+  let tense = getRandom(Array.from(forms?.keys() ?? []))
+  let pronoun = getRandom(Array.from(forms?.get(tense)?.keys() ?? []))
+  // TODO find better way to handle undefined ^^^
+
+  return {
+    verb: verb,
+    tense: tense,
+    pronoun: pronoun
+  }
+}
