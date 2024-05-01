4c695f05a469ed704350b9dc70fb0eb9cef293cd
berci <petoalbert32@gmail.com>
2024-05-01 12:05:05 +0200

    Create basic react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,92 @@
 import React from "react";
+import { useState } from "react";
+import { conjugate } from "./Conjugation";
+import { Tense, tenseToString } from "./Tense";
+import { Pronoun, pronounToString } from "./Pronoun";
+
+type MyButtonProps = {
+  count: number;
+  onClick: () => void;
+}
+
+const verbs = [
+    "actuar",
+    "acentuar",
+    "anticuar",
+    "atenuar",
+    "conceptuar",
+    "consensuar",
+    "continuar",
+    "desconceptuar",
+    "deshabituar",
+    "desvirtuar",
+    "devaluar",
+    "efectuar",
+    "evaluar",
+    "exceptuar",
+    "extenuar",
+    "fluctuar",
+    "graduar",
+    "habituar",
+    "insinuar",
+    "interactuar",
+    "menstruar",
+    "perpetuar",
+    "puntuar",
+    "redituar",
+    "reevaluar",
+    "revaluar",
+    "situar",
+    "tatuar",
+    "valuar"
+]
+
+function getVerb() {
+  const randomIndex = Math.floor(Math.random() * verbs.length);
+  return verbs[randomIndex];
+}
+
+type ConjugationParameters = {
+  verb: string;
+  tense: Tense;
+  pronoun: Pronoun;
+}
+
+function getTense(): Tense {
+  const tenses = [
+    Tense.IndicativoPresente,
+    Tense.IndicativoImperfecto,
+    Tense.IndicativoIndefinido,
+    Tense.IndicativoFuturoImperfecto,
+    Tense.IndicativoCondicional,
+    Tense.SubjuntivoPresente,
+    Tense.ImperativoAfirmativo,
+    Tense.ImperativoNegativo
+  ]
+  const randomIndex = Math.floor(Math.random() * tenses.length);
+  return tenses[randomIndex];
+}
+
+function getPrononun(): Pronoun {
+  const pronouns = [
+    Pronoun.Yo, 
+    Pronoun.Tu, 
+    Pronoun.Usted, 
+    Pronoun.Nosotros, 
+    Pronoun.Vosotros, 
+    Pronoun.Ustedes  
+  ]
+  const randomIndex = Math.floor(Math.random() * pronouns.length);
+  return pronouns[randomIndex];
+}
+
+function getParameters(): ConjugationParameters {
+  return {
+    verb: getVerb(),
+    tense: getTense(),
+    pronoun: getPrononun()
+  }
+}
 
 function Profile() {
     return (
@@ -8,14 +96,29 @@ function Profile() {
       />
     );
   }
+
+  function MyButton( { onClick } : { onClick: () => void } ) {  
+    return (
+      <button onClick={onClick}>
+        New word
+      </button>
+    );
+  }
   
   export default function Gallery() {
+    const [params, setParams] = useState<ConjugationParameters>(getParameters());
+  
+    function handleClick() {
+      setParams(getParameters());
+    }
+
     return (
       <section>
-        <h1>Amazing scientists</h1>
-        <Profile />
-        <Profile />
-        <Profile />
+        <MyButton onClick={handleClick} />
+        <div>{params.verb}</div>
+        <div>{tenseToString(params.tense)}</div>
+        <div>{pronounToString(params.pronoun)}</div>
+        <div>{conjugate(params.verb, params.tense, params.pronoun)}</div>
       </section>
     );
   }
