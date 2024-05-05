ce2d47351176ba3e5d0a4d357856f1b0278dcce0
Albert Peto <petoalbert32@gmail.com>
2024-05-05 14:15:36 +0200

    Trim and convert input to lowercase

--- src/TaskResult.tsx
+++ src/TaskResult.tsx
@@ -4,12 +4,13 @@ import { ConjugationParameters } from "./ConjugationParameters";
 
 export function TaskResult({ params, input }: { params: ConjugationParameters, input: string }) {
     const correctForm = conjugate(params.verb, params.tense, params.pronoun)
+    const trimmedInput = input.toLowerCase().trim()
 
-    if (input === correctForm) {
+    if (trimmedInput === correctForm) {
         return (<p className="success">{correctForm} &#128526;</p>)
     } else if (input.trim() === "") {
         return (<p className="failure">No guess &#128529;<br />{correctForm}</p>)
     } else {
-        return (<p className="failure"><s>{input}</s> &#128546;<br />{correctForm}</p>)
+        return (<p className="failure"><s>{trimmedInput}</s> &#128546;<br />{correctForm}</p>)
     }
 }
