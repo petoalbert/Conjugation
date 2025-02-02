f4c97c99538d40a70d4d141a782f2d13239a831a
berci <petoalbert32@gmail.com>
2025-02-02 14:38:12 +0100

    Add tooltip

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-import { tenseToString } from "../utils/Tense";
+import { getOnlyIrregularParameters } from "../utils/ConjugationParameters";
 import { pronounToString } from "../utils/Pronoun";
-import { ConjugationParameters, getOnlyIrregularParameters } from "../utils/ConjugationParameters";
+import { tenseToString } from "../utils/Tense";
 import Input from "./Input";
-import { conjugate } from "../utils/Conjugation";
-import { TaskResult } from "./TaskResult";
 
 export default function Task() {
   const [params, setParams] = React.useState(getOnlyIrregularParameters(true));
+  const [isTooltipVisible, setIsTooltipVisible] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    setIsTooltipVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsTooltipVisible(false);
+  };
 
   return (
     <div className="text-center">
       <div className="relative bg-white z-[100]">
-        <div className="relative z-[100] font-bold text-xl mb-2">{params.verb}</div>
+        <div className="relative z-[100] font-bold text-xl mb-2">
+          <span className="border-b border-dotted border-black" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            {params.verb}
+          </span>
+        </div>
+        {isTooltipVisible && (
+          <div
+            data-tooltip="tooltip"
+            className="absolute w-[80%] left-[10%] bottom-[70px] z-[150] whitespace-normal break-words rounded-lg bg-black py-1.5 px-3 font-sans text-sm font-normal text-white focus:outline-none"
+          >
+            Sample tooltip
+          </div>
+        )}
         <div className="relative z-[100] text-gray-700 mb-2">
           <h6 className="z-[100] text-lg font-semibold">
             {pronounToString(params.pronoun)} - {tenseToString(params.tense)}
